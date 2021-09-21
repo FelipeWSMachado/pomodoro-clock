30a3c774a7a447dfd598d51fc9bef0ba02252e01
Felipe Machado <phelipecwb@gmail.com>
2021-09-21 19:40:45 -0300

    Finalização do app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import Timer from "./Timer";
+import Settings from "./Settings";
+import SettingsContext from "./SettingsContext";
+
+import "./App.css";
+
+function App() {
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
+  return (
+    <main>
+      <SettingsContext.Provider
+        value={{
+          showSettings,
+          setShowSettings,
+          workMinutes,
+          breakMinutes,
+          setWorkMinutes,
+          setBreakMinutes,
+        }}
+      >
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Settings.js
@@ -0,0 +1,37 @@
+import { useContext } from "react";
+import ReactSlider from "react-slider";
+import SettingsContext from "./SettingsContext";
+import BackButton from "./BackButton";
+
+import "./slider.css";
+
+export default function Settings() {
+  const settingsInfo = useContext(SettingsContext);
+  return (
+    <div style={{ textAlign: "left" }}>
+      <label>Trabalhar por: {settingsInfo.workMinutes}:00</label>
+      <ReactSlider
+        className={"slider"}
+        thumbClassName={"thumb"}
+        trackClassName={"track"}
+        value={settingsInfo.workMinutes}
+        onChange={(newValue) => settingsInfo.setWorkMinutes(newValue)}
+        min={1}
+        max={120}
+      />
+      <label>Pausa de: {settingsInfo.breakMinutes}:00</label>
+      <ReactSlider
+        className={"slider green"}
+        thumbClassName={"thumb"}
+        trackClassName={"track"}
+        value={settingsInfo.breakMinutes}
+        onChange={(newValue) => settingsInfo.setBreakMinutes(newValue)}
+        min={1}
+        max={120}
+      />
+      <div style={{ textAlign: "center", marginTop: "20px" }}>
+        <BackButton onClick={() => settingsInfo.setShowSettings(false)} />
+      </div>
+    </div>
+  );
+}
